b470f7552418a7526e65384116680e36e919af76
David Meister <thedavidmeister@gmail.com>
2017-05-13 17:21:20 +1000

    added basic geometry for polar co-ordinates

--- src/wheel/core.clj
+++
@@ -1,6 +0,0 @@
-(ns wheel.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
---
+++ src/wheel/math/geometry.cljs
@@ -0,0 +1,17 @@
+(ns wheel.math.geometry)
+
+(defn polar->cartesian
+ "Given a radius (unitless) and rotation in radians, returns (unitless) [x y] co-ordinates"
+ [radius radians]
+ [(* radius (.cos js/Math radians))
+  (* radius (.sin js/Math radians))])
+
+(defn degrees->radians
+ "Given a rotation in degrees, returns the same rotation in radians"
+ [degrees]
+ (/ (* degrees (.-PI js/Math)) 180))
+
+(defn radians->degrees
+ "Given a rotation in radians, returns the same rotation in degrees" 
+ [radians]
+ (/ (* radians 180) (.-PI js/Math)))
