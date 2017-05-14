7ed7c9858ccefbda8fefd29cd353ecd8d3215b3f
David Meister <thedavidmeister@gmail.com>
2017-05-14 13:42:52 +1000

    fonts->uri-str and ??fonts->uri-str

--- src/wheel/hoplon/google_fonts/api.cljs
+++ src/wheel/hoplon/google_fonts/api.cljs
@@ -1,6 +1,6 @@
 (ns wheel.hoplon.google-fonts.api
  (:require
-  [cljs.test :refer-macros [deftest is are]]))
+  [cljs.test :refer-macros [deftest is]]))
 
 ; Most Google Fonts functions work with hash maps representing a font.
 ; The keys are as follows:
@@ -17,14 +17,32 @@
                      (str ":" (clojure.string/join "," variants)))]
   (str name-uri variants-uri)))
 
+(defn fonts->uri-str
+ "Given a sequence of fonts, returns a string suitable in a Google Fonts URI"
+ [fonts]
+ {:pre [(sequential? fonts)]}
+ (clojure.string/join "|" (map font->uri-str fonts)))
+
 ; TESTS
 
+(def examples
+ (partition 2
+  [{:name ""} ""
+   {:name "foo"} "foo"
+   {:name "foo bar"} "foo+bar"
+   {:name "foo" :variants []} "foo"
+   {:name "foo" :variants ["1"]} "foo:1"
+   {:name "foo" :variants ["1" "2"]} "foo:1,2"
+   {:name "foo bar" :variants ["1" "2"]} "foo+bar:1,2"]))
+
 (deftest ??font->uri-str
- (are [i o] (= o (font->uri-str i))
-  {:name ""} ""
-  {:name "foo"} "foo"
-  {:name "foo bar"} "foo+bar"
-  {:name "foo" :variants []} "foo"
-  {:name "foo" :variants ["1"]} "foo:1"
-  {:name "foo" :variants ["1" "2"]} "foo:1,2"
-  {:name "foo bar" :variants ["1" "2"]} "foo+bar:1,2"))
+ (doseq [[i o] examples]
+  (is (= o (font->uri-str i)))))
+
+(deftest ??fonts->uri-str
+ (is (= "" (fonts->uri-str [])))
+ (let [[i o] (rand-nth examples)]
+  (is (= o (fonts->uri-str [i]))))
+ (let [[i o] (rand-nth examples)
+       [i' o'] (rand-nth examples)]
+  (is (= (str o "|" o') (fonts->uri-str [i i'])))))
