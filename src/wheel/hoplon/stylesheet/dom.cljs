387ea11e225eb49d45699791b7dcd0bc7986a717
David Meister <thedavidmeister@gmail.com>
2017-05-14 14:57:28 +1000

    lint

--- src/wheel/hoplon/stylesheet/dom.cljs
+++ src/wheel/hoplon/stylesheet/dom.cljs
@@ -4,6 +4,7 @@
   [cljs.test :refer-macros [deftest is]]))
 
 (defn link [href]
+ "Given an href returns a link with the correct attributes for a stylesheet"
  (h/link
   :href href
   :rel "stylesheet"
