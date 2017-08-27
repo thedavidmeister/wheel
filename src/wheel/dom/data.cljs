f10a8fe5cdd626e862977035efb5b95b49f9ef62
David Meister <thedavidmeister@gmail.com>
2017-08-27 22:52:47 +1000

    add basic generator for :wheel.dom/element

--- src/wheel/dom/data.cljs
+++ src/wheel/dom/data.cljs
@@ -1,9 +1,20 @@
 (ns wheel.dom.data
  (:require
+  [hoplon.core :as h]
   goog.dom
-  [clojure.spec.alpha :as spec]))
+  [clojure.spec.alpha :as spec]
+  [cljs.test :refer-macros [deftest is]]
+  [clojure.test.check.generators :as gen]))
 
-(spec/def :wheel.dom/element goog.dom/isElement)
+(spec/def :wheel.dom/element
+ (spec/spec
+  goog.dom/isElement
+  :gen
+  (constantly
+   (gen/fmap
+    apply
+    (gen/elements [h/div h/span h/p])))))
+
 (spec/def :wheel.dom/selector string?)
 
 (defn el? [el] (spec/valid? :wheel.dom/element el))
